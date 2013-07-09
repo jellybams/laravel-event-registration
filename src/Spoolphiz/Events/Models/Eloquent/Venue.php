9b252379bc6e68c4651bdf0c6bfacd5e893327cc
spoolphiz <alexander.manea@gmail.com>
2013-07-09 16:35:27 -0700

    adding venue deletion and events relation

--- src/Spoolphiz/Events/Models/Eloquent/Venue.php
+++ src/Spoolphiz/Events/Models/Eloquent/Venue.php
@@ -1,5 +1,6 @@
 <?php
 namespace Spoolphiz\Events\Models\Eloquent;
+use \DB;
 use \Eloquent;
 use \Validator;
 use \ValidationException;
@@ -47,13 +48,19 @@ class Venue extends Eloquent {
 	}
 	
 	
+	public function events()
+	{
+		return $this->hasMany('Spoolphiz\Events\Models\Eloquent\Event');
+	}
+	
+	
 	/**
 	 * Validate the model's attributes.
 	 *
 	 * @return void
 	 */
 	public function validate() 
-	{
+	{		
 		$val = Validator::make($this->attributes, $this->validators);
 
 		if ($val->fails())
@@ -61,6 +68,35 @@ class Venue extends Eloquent {
 			throw new ValidationException($val);
 		}
 	}
+	
+	
+	
+	/**
+	 * Delete the given model. When a venue is deleted, all events that were associated to
+	 * it will be instead transferred to the 'no venue' record (venue_id = 1)
+	 *
+	 * @return bool
+	 */
+	public function delete() 
+	{
+		//don't allow deletion of the 'no venue' venue
+		if( !$this->allowModify() )
+		{
+			App::abort('This venue cannot be modified/deleted.', 403);
+		}
+		
+		//get a list of events associated to this venue
+		$relatedEventIds = $this->events()->lists('id');
+		
+		//update the venue id to 1 for the events
+		if( !empty($relatedEventIds) )
+		{
+			$result = DB::table('events')->whereIn('id', $relatedEventIds)->update(array('venue_id' => 1));
+		}
+
+		return parent::delete();
+	}
+	
 	
 	
 	/**
@@ -94,5 +130,25 @@ class Venue extends Eloquent {
 		}
 
 		return false;
+	}
+	
+	
+	/**
+	 * Checks if this model is allowed to be deleted/modified. As of now only disables
+	 * DELETE and PUT operations on the 'no venue' record. 
+	 * 
+	 * NOTE: This is not an ACL/permissions check.
+	 *
+	 *
+	 * @return bool
+	 */
+	public function allowModify()
+	{	
+		if( $this->id == 1 )
+		{
+			return false;
+		}
+		
+		return true;
 	}
 }
